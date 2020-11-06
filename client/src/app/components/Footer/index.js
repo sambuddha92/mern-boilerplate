537b0c6875bf1f6672cbd87ba7ce08491aebe7b4
sambuddha92 <sambuddhaadhikari@gmail.com>
2020-11-06 15:51:31 +0530

    footer links

--- client/src/app/components/Footer/index.js
+++ client/src/app/components/Footer/index.js
@@ -5,8 +5,8 @@ const Footer = () => {
     return (
         <footer className="Footer">
             <div className="inner container is-fixed">
-                <a className="no-deco" href="https://github.com/sambuddha92/pern-boilerplate" target="_blank" rel="noopener noreferrer" >Source Code</a>
-                <a className="no-deco" href="https://github.com/sambuddha92/pern-boilerplate" target="_blank" rel="noopener noreferrer" >My Github</a>
+                <a className="no-deco" href="https://github.com/sambuddha92/mern-boilerplate" target="_blank" rel="noopener noreferrer" >Source Code</a>
+                <a className="no-deco" href="https://github.com/sambuddha92" target="_blank" rel="noopener noreferrer" >My Github</a>
                 <p>© 2020 everyone in the universe.</p>
             </div>
         </footer>
